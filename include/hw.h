1f835d7c52867e449607664ba179fcd43bdf5acf
Bernhard Bablok <bablokb@gmx.de>
2021-05-24 07:31:24 +0200

    renamed SPI_CS to TFT_CS

--- include/hw.h
+++ include/hw.h
@@ -16,11 +16,11 @@
 // pin and ports (usually defined in the makefile)
 
 // chip-select output pin
-#ifndef PIN_SPI_CS
-  #define PIN_SPI_CS 4
+#ifndef PIN_TFT_CS
+  #define PIN_TFT_CS 4
 #endif
-#ifndef PORT_SPI_CS
-  #define PORT_SPI_CS A
+#ifndef PORT_TFT_CS
+  #define PORT_TFT_CS A
 #endif
 
 // TFT_DC output pin
@@ -43,9 +43,14 @@
 // ----------------------------------------------------------------
 // helper macros
 
+#define ANSEL_TFT_CS _CONCAT(ANSEL,PORT_TFT_CS)
+#define TRIS_TFT_CS  _CONCAT(TRIS,PORT_TFT_CS)
+#define GP_TFT_CS    _CONCAT(R,_CONCAT(PORT_TFT_CS,PIN_TFT_CS))
+
 #define ANSEL_TFT_DC _CONCAT(ANSEL,PORT_TFT_DC)
 #define TRIS_TFT_DC  _CONCAT(TRIS,PORT_TFT_DC)
 #define GP_TFT_DC    _CONCAT(R,_CONCAT(PORT_TFT_DC,PIN_TFT_DC))
+
 #define ANSEL_TFT_RST _CONCAT(ANSEL,PORT_TFT_RST)
 #define TRIS_TFT_RST  _CONCAT(TRIS,PORT_TFT_RST)
 #define GP_TFT_RST    _CONCAT(R,_CONCAT(PORT_TFT_RST,PIN_TFT_RST))
@@ -65,8 +70,8 @@
 
 #define spiwrite(data)             spi_write(data)
 
-#define spi_cs_low()               GP_SPI_CS = 0
-#define spi_cs_high()              GP_SPI_CS = 1
+#define tft_cs_low()               GP_TFT_CS = 0
+#define tft_cs_high()              GP_TFT_CS = 1
 
 #define tft_dc_low()               GP_TFT_DC = 0
 #define tft_dc_high()              GP_TFT_DC = 1
